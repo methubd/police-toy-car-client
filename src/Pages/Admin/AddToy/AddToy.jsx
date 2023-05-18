f595fbb8e307a66c6cfccd4710b842862557b7ed
T R Methu <fmethubd@gmail.com>
2023-05-18 16:00:06 +0600

    Add toy button is working now

--- src/Pages/Admin/AddToy/AddToy.jsx
+++ src/Pages/Admin/AddToy/AddToy.jsx
@@ -1,12 +1,106 @@
 import React from 'react';
+import Swal from 'sweetalert2';
 
 const AddToy = () => {
+
+    const handleAddToy = event => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const subCategory = form.subCategory.value;
+        const seller = form.seller.value;
+        const quantity = form.quantity.value;
+        const email = form.email.value;
+        const price = form.price.value;
+        const rating = form.rating.value;
+        const image = form.image.value;
+        const details = form.details.value;
+        const newToy = {name, subCategory, seller, quantity, email, price, rating, image, details};
+
+        fetch('http://localhost:5000/toys', {
+            method: 'POST',
+            headers: {
+                "content-type" : "application/json"
+            },
+            body: JSON.stringify(newToy)
+        })
+        .then(res => res.json())
+        .then(data => {
+            if(data.insertedId){
+                Swal.fire({
+                    position: 'center',
+                    icon: 'success',
+                    title: 'Successfully Added',
+                    showConfirmButton: false,
+                    timer: 1500
+                  })
+            }
+        })
+
+    }
     return (
         <div>
-            <h1 className='font-semibold text-2xl text-center py-2 bg-blue-100 text-gray-600'>Add a Toy</h1>
+            <h1 className='font-semibold text-2xl text-center py-2 bg-purple-100 text-gray-600'>Add a Toy</h1>
             <div>
-                <form>
-                    <input type="text" name="" id="" />
+                <form onSubmit={handleAddToy} className='text-center mt-10'>
+
+                    <input className='p-2 bg-purple-50 md:w-2/3 border border-gray-300 my-1' 
+                    type="text" 
+                    name="name" 
+                    placeholder='Please type ( ... Toy Name )' required/>
+                    
+                    <div className=' md:w-2/3 mx-auto md:flex my-2'>
+                        <input className='p-2 bg-purple-50 md:w-2/3 border border-gray-300 md:me-5' 
+                        type="text" 
+                        name="subCategory" 
+                        placeholder='Sub Category ( ... Jeep, Regular )' />
+                        
+                        <input className='p-2 bg-purple-50 md:w-2/3 border border-gray-300' 
+                        type="text" 
+                        name="seller" 
+                        placeholder='Seller Name' />
+                    </div>
+
+                    <div className=' md:w-2/3 mx-auto md:flex my-2'>
+                        <input className='p-2 bg-purple-50 md:w-2/3 border border-gray-300 md:me-5' 
+                        type="text" 
+                        name="quantity" 
+                        placeholder='Available Quantity' />
+
+                        <input className='p-2 bg-purple-50 md:w-2/3 border border-gray-300 ' 
+                        type="text" 
+                        name="email" 
+                        placeholder='Seller Email ( ...@email.com )' />
+                        
+                    </div>
+
+                    <div className=' md:w-2/3 mx-auto md:flex my-2'>
+                        <input className='p-2 bg-purple-50 md:w-2/3 border border-gray-300 md:me-5' 
+                        type="text" 
+                        name="price" 
+                        placeholder='Price ( .../product )' />
+
+                        <input className='p-2 bg-purple-50 md:w-2/3 border border-gray-300 ' 
+                        type="text" 
+                        name="rating" 
+                        placeholder='Ratings (... 1 - 5)' />
+                        
+                    </div>
+
+                        <input className='p-2 bg-purple-50 md:w-2/3 border border-gray-300 my-1' 
+                        type="text" 
+                        name="image" 
+                        placeholder='Photo URL ( ...https:// )' required/>
+
+                    <div className=''>
+                        <textarea className='p-2 bg-purple-50 md:w-2/3 border border-gray-300 my-1' 
+                        type="text" 
+                        name="details"
+                        cols="30" rows="5" 
+                        placeholder='Type details abut your prodcut.' required/> <br />
+
+                        <input className='p-2 px-10 text-lg bg-purple-500 text-white' type="submit" value="Add" />
+                    </div>
                 </form>
             </div>
         </div>
